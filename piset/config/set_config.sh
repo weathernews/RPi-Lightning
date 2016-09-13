53bd5e773affac725e67643200de8819d8aae0ad
yuh <yuh@wni.com>
2016-09-13 23:49:15 +0900

    update

--- piset/config/set_config.sh
+++ piset/config/set_config.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
+MYPATH=`dirname $0`
 if [ "$1" != "" ]; then
     CONFIG=$1
 else
     CONFIG=self_ap
 fi
 
-(cd $CONFIG ; tar cfp - .) | (cd / ; tar xf -)
+(cd $MYPATH/$CONFIG ; tar cfp - .) | (cd / ; tar xf -)
 
 
