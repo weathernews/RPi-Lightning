9d98f0e35b1312a3cf95036f2d25abcb82ac228e
Yuichiro Nishi <yuh@wni.com>
2016-12-20 14:51:53 +0900

    add target

--- piset/init.sh
+++ piset/init.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 MYPATH=`dirname $0`
 /bin/rm -f $MYPATH/../htdocs/network.json
+/bin/rm -f $MYPATH/../htdocs/wlan.txt
+/bin/rm -f $MYPATH/../htdocs/location.json
 /bin/rm -f $MYPATH/../lightning/*.pyc
 /bin/rm -f $MYPATH/../lightning/id.py
 /bin/rm -f $MYPATH/../lightning/proxy.py
